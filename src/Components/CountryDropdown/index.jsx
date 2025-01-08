2afdd667a9494f83c21b7b6e167d5569e5a56db3
Joel <joelpercca3@gmail.com>
2025-01-08 11:25:58 -0500

    Blog de menu deslizante agragado

--- src/Components/CountryDropdown/index.jsx
+++ src/Components/CountryDropdown/index.jsx
@@ -1,18 +1,79 @@
+import React from 'react';
 import Button from '@mui/material/Button';
 import { FaAngleDown } from "react-icons/fa6";
+import Dialog from '@mui/material/Dialog';
+import { IoIosSearch } from "react-icons/io";
+import { MdClose } from "react-icons/md";
+import { useState } from 'react';
+import Slide from '@mui/material/Slide';
+
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 
 
 
 const CountryDropdown = () => {
+
+  const [isOpenModal, setisOpenModal] = useState(false);
+
+
   return (
     <>
-      <Button className="countryDrop">
+      <Button className="countryDrop" onClick={() => setisOpenModal(true)}>
         <div className="info d-flex flex-column">
           <span className='label'>Your Location</span>
           <span className='name'>India</span>
         </div>
-        <span className='ml-auto'><FaAngleDown/></span>
+        <span className='ml-auto'><FaAngleDown /></span>
       </Button>
+
+      <Dialog open={isOpenModal} onClose={() => setisOpenModal(false)}
+        className='locationModal' TransitionComponent={Transition}
+
+      >
+        <h4 className='mb-0'>Choose your Delivery Location</h4>
+        <p>Enter your address and we will specify the ofter for your area.</p>
+        <Button className='close_' onClick={() => setisOpenModal(false)}><MdClose /></Button>
+
+        <div className='headerSearch w-100'>
+          <input type='text' placeholder='Search your area...' />
+          <Button>
+            <IoIosSearch />
+          </Button>
+        </div>
+
+        <ul className='countryList mt-3'>
+          <li><Button onClick={() => setisOpenModal(false)}>India</Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Sri Lanka </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Pakistan </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Pakistan </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Pakistan </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Pakistan </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>India</Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Sri Lanka </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Pakistan </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Pakistan </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Pakistan </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Pakistan </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>India</Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Sri Lanka </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Pakistan </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Pakistan </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Pakistan </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Pakistan </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Sri Lanka </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Pakistan </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Pakistan </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Pakistan </Button></li>
+          <li><Button onClick={() => setisOpenModal(false)}>Pakistan </Button></li>
+
+
+        </ul>
+
+      </Dialog>
     </>
   );
 };
