8c78b9cd9f7f7d6bc4422cc9cdb8e9d3db5bd555
Joel <joelpercca3@gmail.com>
2025-01-21 11:32:22 -0500

    commit #9 terminado

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,24 @@
 import HomeBanner from "../../Components/HomeBanner";
+import banner1 from "../../assets/images/banner1.jpg";
+import Button from "@mui/material/Button";
+import { IoMdArrowRoundForward } from "react-icons/io";
+import React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import ProductItem from "../../Components/ProductItem/ProductItem";
 
 const Home = () => {
+
+  var productSliderOptions = {
+    dots: true,
+    infinite: false,
+    speed: true,
+    slidesToShow: 4,
+    slidesToScroll: 1
+  };
+
   return (
     <>
       <HomeBanner />
@@ -10,21 +28,113 @@ const Home = () => {
           <div className="row">
             <div className="col-md-3">
               <div className="banner">
-                <img src="https://api.spicezgold.com/download/file_1734525757507_NewProject(34).jpg"
-                  className="cursor" />
+                <img src={banner1} className="cursor w-100" />
               </div>
-
             </div>
 
-            <div className="col-md-3">
+            <div className="col-md-9 productRow w-100">
               <div className="d-flex align-items-center">
-                <div className="info">
-                  <h3>BEST SELLERS</h3>
-                  <p className="text-light">Do not miss the current offers
-                    until the end of March.</p>
+                <div className="info w-75">
+                  <h3 className="mb-0 hd">BEST SELLERS</h3>
+                  <p className="text-light text-sml mb-0">
+                    Do not miss the current offers until the end of March.
+                  </p>
                 </div>
+                <Button className="viewAllBtn ml-auto">
+                  View All
+                  <IoMdArrowRoundForward />
+                </Button>
               </div>
+
+              <div className="product_row w-100 mt-4">
+                <Swiper
+                  slidesPerView={4}
+                  spaceBetween={0}
+                  pagination={{
+                    clickable: true
+                  }}
+                  modules={[Navigation]}
+                  className="mySwiper"
+                >
+                  <SwiperSlide>
+                    <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <ProductItem/>
+                  </SwiperSlide>
+
+                
+                </Swiper>
+              </div>
+
+
+
+              <div className="d-flex align-items-center mt-5">
+                <div className="info w-75">
+                  <h3 className="mb-0 hd">NEW PRODUCTS</h3>
+                  <p className="text-light text-sml mb-0">
+                  New products with updated stocks.
+                  </p>
+                </div>
+                <Button className="viewAllBtn ml-auto">
+                  View All
+                  <IoMdArrowRoundForward />
+                </Button>
+              </div>
+
+              <div className="product_row w-100 mt-4">
+                <Swiper
+                  slidesPerView={4}
+                  spaceBetween={0}
+                  pagination={{
+                    clickable: true
+                  }}
+                  modules={[Navigation]}
+                  className="mySwiper"
+                >
+                  <SwiperSlide>
+                    <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <ProductItem/>
+                  </SwiperSlide>
+
+                
+                </Swiper>
+              </div>
+
             </div>
+
           </div>
         </div>
       </section>
